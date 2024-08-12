3300e7815b87d4de99b4f2442a3a53c4e5ecf695
Gabriele Bologna <bolognagabriele2003@gmail.com>
2024-08-12 13:09:09 +0200

    First commit

---
+++ android/app/src/main/kotlin/me/gabrielebologna/detectability/MainActivity.kt
@@ -0,0 +1,5 @@
+package me.gabrielebologna.detectability
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:detectability/pages/main_page.dart';
+import 'package:detectability/constants.dart' as constants;
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: constants.appName,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
